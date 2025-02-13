42bef6a80a66855f84fad0cfbdb6f4e8d15229da
Yerlenturn <Onion@cumguzzler42069.localdomain>
2025-02-13 14:45:26 -0800

    mo stuff

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -45,6 +45,7 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
+        { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
@@ -52,10 +53,12 @@ return {
 
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
+        fields = { 'menu', 'abbr', 'kind'},
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "...",
         }),
+        expandable_indicator = true,
       },
     })
   end,
